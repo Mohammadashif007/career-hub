6a553d2526705278e6f16555ff91d5b03a6bcad5
Mohammad Ashif <Mohammad Ashif>
2024-03-14 06:06:37 +0600

    navbar and footer added

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root";
+import Home from "./components/Home/Home";
+import Statistics from "./components/Statistics/Statistics";
+import Applied_Jobs from "./components/Applied_Jobs/Applied_Jobs";
+import Blog from "./components/Blog/Blog";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        children: [
+          {
+            path: "/",
+            element: <Home></Home>
+          },
+          {
+            path: "/statistics",
+            element: <Statistics></Statistics>
+          },
+          {
+            path: "/Applied_Jobs",
+            element: <Applied_Jobs></Applied_Jobs>
+          },
+          {
+            path: "/blog",
+            element: <Blog></Blog>
+          }
+        ]
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
